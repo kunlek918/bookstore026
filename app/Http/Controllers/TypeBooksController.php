f9a4d762d9f880a523d99a14b370875dea5d2caa
kunlek918 <60481200+kunlek918@users.noreply.github.com>
2020-03-06 15:56:13 +0700

    add database

--- app/Http/Controllers/TypeBooksController.php
+++ app/Http/Controllers/TypeBooksController.php
@@ -9,12 +9,12 @@ class TypeBooksController extends Controller
 {
     public function index(){
         //$typebooks = TypeBooks::all(); //แสดงข้อมูลทั้งหมด
-        //typebooks = TypeBooks::orderBy('id','desc')->get(); //เเสดงข้อมูลทั้งหมดเรียงจากมาไปน้อยโดยใช้id
+        //$typebooks = TypeBooks::orderBy('id','desc')->get(); //เเสดงข้อมูลทั้งหมดเรียงจากมาไปน้อยโดยใช้id
         $count = TypeBooks::count(); //นับจำนวณเเถวทั้งหมด
-        
+
         //แบ่งหน้า
         $typebooks = TypeBooks::paginate(3);
-        
+
         return view('typebooks.index',[
             'typebooks' => $typebooks,
             'count' => $count
